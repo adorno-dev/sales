8d0523791c7ede10cd13048a10a45c5086be8ae1
adorno-dev <adorno.dev@gmail.com>
2026-08-07 02:34:27 -0300

    Customer Address & Tests

---
+++ src/Sales.Domain.Tests/Customers/Entities/AdressTests.cs
@@ -0,0 +1,57 @@
+﻿using FluentAssertions;
+using Sales.Domain.Common.Exceptions;
+using Sales.Domain.Customers.Entities;
+
+namespace Sales.Domain.Tests.Customers.Entities;
+
+public class AdressTests
+{
+    private static Address CreateValidAddress()
+    {
+        return new Address
+        (
+            postalCode: "12345-678",
+            city: "Los Angeles",
+            street: "Main Street",
+            neighborhood: "Main Neighborhood",
+            state: "CA",
+            country: "United States"
+        );
+    }
+
+    [Fact]
+    public void Should_Create_Valid_Address()
+    {
+        // arrange & act
+        var address = CreateValidAddress();
+        // assert
+        address.PostalCode.Should().Be("12345-678");
+        address.Street.Should().Be("Main Street");
+        address.Neighborhood.Should().Be("Main Neighborhood");
+        address.State.Should().Be("CA");
+        address.City.Should().Be("Los Angeles");
+        address.Country.Should().Be("United States");
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData(" ")]
+    public void Should_Throw_Exception_When_PostalCode_Is_Invalid(string? postalCode)
+    {
+        // arrange
+        Action act = () => new Address
+        (
+            postalCode: postalCode!,
+            city: "Los Angeles",
+            street: "Main Street",
+            neighborhood: "Main Neighborhood",
+            state: "CA",
+            country: "United States"
+        );
+
+        // assert
+        act.Should().Throw<DomainException>()
+           .WithMessage("Postal code is required.");
+    }
+}
---
+++ src/Sales.Domain/Customers/Entities/Address.cs
@@ -0,0 +1,79 @@
+﻿using Sales.Domain.Common.Base;
+using Sales.Domain.Common.Exceptions;
+using Sales.Domain.Common.Validations;
+using System.Text.RegularExpressions;
+
+namespace Sales.Domain.Customers.Entities;
+
+public sealed class Address : Entity
+{
+    public string PostalCode { get; private set; }
+    public string Street { get; private set; }
+    public string? Complement { get; private set; }
+    public string Neighborhood { get; private set; }
+    public string State { get; private set; }
+    public string City { get; private set; }
+    public string Country { get; private set; }
+
+    public Address
+    (
+        string postalCode, 
+        string street, 
+        string neighborhood, 
+        string state, 
+        string city, 
+        string country,
+        string? complement = null
+    )
+    {
+        PostalCode = postalCode;
+        Street = street;
+        Neighborhood = neighborhood;
+        State = state;
+        City = city;
+        Country = country;
+        Complement = complement;
+    }
+
+    internal void Update
+    (
+        string postalCode,
+        string street,
+        string neighborhood,
+        string state,
+        string city,
+        string country,
+        string? complement = null
+    )
+    {
+        Validate(postalCode, street, neighborhood, city, state, country);
+
+        PostalCode = postalCode;
+        Street = street;
+        Neighborhood = neighborhood;
+        State = state;
+        City = city;
+        Country = country;
+        Complement = complement;
+    }
+
+    private void Validate
+    (
+        string postalCode, 
+        string street, 
+        string neighborhood, 
+        string city, 
+        string state, 
+        string country
+    )
+    {
+        Guard.AgainstNullOrWhiteSpace(postalCode, nameof(postalCode), "Postal code is required.");
+        Guard.Against<DomainException>(!Regex.IsMatch(PostalCode, @"^\d{8}$"), "Invalid postal code format.");
+        Guard.AgainstNullOrWhiteSpace(street, nameof(street), "Street is required.");
+        Guard.Against<DomainException>(street.Length < 3, "Street must be at least 3 characters long.");
+        Guard.AgainstNullOrWhiteSpace(neighborhood, nameof(neighborhood), "Neighborhood is required.");
+        Guard.AgainstNullOrWhiteSpace(state, nameof(state), "State is required.");
+        Guard.AgainstNullOrWhiteSpace(city, nameof(city), "City is required.");
+        Guard.AgainstNullOrWhiteSpace(country, nameof(country), "Country is required.");
+    }
+}
